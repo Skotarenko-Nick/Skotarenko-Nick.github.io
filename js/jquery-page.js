7b4ce508616bdd4d3fb65cc5464f0a758fcdb77b
Nick-Skotarenko <nick.skotarenko@gmail.com>
2020-10-27 21:34:44 +0100

    Fixed html. Added copying of email instead of emailing

--- js/jquery-page.js
+++ js/jquery-page.js
@@ -1,6 +1,7 @@
-    $(document).ready(function () {
-    	$(".reverse-version").click(function () {
-    		$("card-text").css("color", "black");
-    		$("body").css("background-color", "black");
-    	});
-    });
+//Left as example
+    // $(document).ready(function () {
+    // 	$(".reverse-version").click(function () {
+    // 		$("card-text").css("color", "black");
+    // 		$("body").css("background-color", "black");
+    // 	});
+    // });
